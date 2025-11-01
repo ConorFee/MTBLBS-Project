9587f286d3c0b5ca134c1a4ebe675ec1026556f1
Conor <C22414306@mytudublin.ie>
2025-11-01 18:39:37 +0000

    MTBLBS: updated views.py, view type changed to generic import

--- mtb_trails/views.py
+++ mtb_trails/views.py
@@ -1,29 +1,50 @@
-from rest_framework import viewsets, permissions
-from rest_framework_gis.filters import DistanceToPointFilter, InBBoxFilter
+from rest_framework import generics, permissions
+from rest_framework_gis.filters import InBBoxFilter, DistanceToPointFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Trail, POI, Park
 from .serializers import TrailSerializer, POISerializer, ParkSerializer
 
-class TrailViewSet(viewsets.ModelViewSet):
+# Trails Views
+class TrailListCreateView(generics.ListCreateAPIView):
     queryset = Trail.objects.all()
     serializer_class = TrailSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # Added for Spatial filtering
     filter_backends = [DjangoFilterBackend, InBBoxFilter]
-    bbox_filter_field = 'path'
+    bbox_filter_field = 'path'  # LineStringField for trail paths
 
-class POIViewSet(viewsets.ModelViewSet):
+class TrailDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Trail.objects.all()
+    serializer_class = TrailSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# POI Views
+class POIListCreateView(generics.ListCreateAPIView):
     queryset = POI.objects.all()
     serializer_class = POISerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # Spatial filtering
     filter_backends = [DjangoFilterBackend, DistanceToPointFilter, InBBoxFilter]
-    bbox_filter_field = 'location'
-    distance_filter_field = 'location'
+    bbox_filter_field = 'location'  # PointField for map bounds filtering
+    distance_filter_field = 'location'  # Enables distance queries
     distance_filter_convert_meters = True
 
-class ParkViewSet(viewsets.ModelViewSet):
+class POIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = POI.objects.all()
+    serializer_class = POISerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# Park Views
+class ParkListCreateView(generics.ListCreateAPIView):
     queryset = Park.objects.all()
     serializer_class = ParkSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # Spatial filtering
     filter_backends = [DjangoFilterBackend, InBBoxFilter]
-    bbox_filter_field = 'boundary'
+    bbox_filter_field = 'boundary'  # PolygonField for park boundaries
+
+class ParkDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Park.objects.all()
+    serializer_class = ParkSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
