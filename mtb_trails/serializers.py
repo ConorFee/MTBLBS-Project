cba6513fe6c5a15303eaf6aa810803957c3b00f0
Conor <C22414306@mytudublin.ie>
2025-11-01 17:33:28 +0000

    MTBLBS: updated rest-framework (gis), update dep in reqiurements.txt

--- mtb_trails/serializers.py
+++ mtb_trails/serializers.py
@@ -1,20 +1,20 @@
-from rest_framework import GeoFeatureModelSerializer
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from .models import Trail, POI, Park
 
 class TrailSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = Trail
-        geo_field = 'path'
+        geo_field = 'path'  # LineString (geometry)
         fields = '__all__'
 
 class POISerializer(GeoFeatureModelSerializer):
     class Meta:
         model = POI
-        geo_field = 'location'
+        geo_field = 'location'  # Point (geometry)
         fields = '__all__'
 
 class ParkSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = Park
-        geo_field = 'boundary'
+        geo_field = 'boundary'  # Polygon (geometry)
         fields = '__all__'
