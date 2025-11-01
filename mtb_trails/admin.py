bb2505f37cfd8da808a11fe861fde6718a9e85c0
Conor <C22414306@mytudublin.ie>
2025-11-01 20:53:14 +0000

    MTBLBS: registered models in admin.py, setup superuser, tested /admin/ page

--- mtb_trails/admin.py
+++ mtb_trails/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Trail, POI, Park
 
-# Register your models here.
+@admin.register(Trail)
+class TrailAdmin(admin.ModelAdmin):
+    list_display = ('name', 'difficulty', 'length_km', 'elevation_gain_m')
+    list_filter = ('difficulty',)
+    search_fields = ('name',)
+
+@admin.register(POI)
+class POIAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type')
+    list_filter = ('type',)
+    search_fields = ('name',)
+
+@admin.register(Park)
+class ParkAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
