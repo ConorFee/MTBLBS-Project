63f8deb0a065a27958d957da718fcec4ad414318
Conor <C22414306@mytudublin.ie>
2025-11-01 13:07:56 +0000

    MTBLBS: first database migrations made, basic models defined

--- mtb_trails/models.py
+++ mtb_trails/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Trail(models.Model):
+    name = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=20, choices=[('beginner', 'Beginner'), ('intermediate', 'Intermediate'), ('expert', 'Expert')])
+    length_km = models.FloatField()
+    elevation_gain_m = models.FloatField()
+    path = models.LineStringField(srid=4326, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+class POI(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50, choices=[('bike_shop', 'Bike Shop'), ('parking', 'Parking'), ('trailhead', 'Trailhead')])
+    location = models.PointField(srid=4326, db_index=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
+
+class Park(models.Model):
+    name = models.CharField(max_length=100)
+    boundary = models.PolygonField(srid=4326, db_index=True)
+
+    def __str__(self):
+        return self.name
